7c8f0c26ff3f4053c4a97b15ef2335b65a851a6d
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2022-03-09 11:06:12 +0000

    search.html
    search html currently not working, will need more time to implement

--- DemoWebsiteNew/Output/outputscript.js
+++ DemoWebsiteNew/Output/outputscript.js
@@ -19,22 +19,23 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
   }
   
-  //search function implementation
   function myFunction() {
     // Declare variables
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("mySearch");
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myMenu");
-    li = ul.getElementsByTagName("li");
+    ul = document.getElementById("tab");
+    li = ul.getElementsByTagName('li');
   
     // Loop through all list items, and hide those who don't match the search query
-    for   (i = 0; i < li.length; i++) {
+    for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
         li[i].style.display = "";
       } else {
         li[i].style.display = "none";
       }
     }
   }
+  
