e44b3b02e9fa111507f1e3eda2c15df490981016
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2022-04-10 13:01:18 +0100

    removal of files
    these files already belong in the main branch

--- SimpleTranslator/TranslatorOne.py
+++
@@ -1,6 +0,0 @@
-from deep_translator import GoogleTranslator
-
-to_translate = "je veux traduire"
-
-translated = GoogleTranslator(source='auto',target='en').translate(to_translate)
-print(translated)
--- test_sample.py
+++
@@ -1,7 +0,0 @@
-import test_sample
-
-def func(x):
-    return x + 1
-
-def test_answer():
-    assert func(3) == 4
