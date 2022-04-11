74a959317bfb7f7e7ffbd937c5ebc3d7aa6b426b
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2022-04-11 16:25:19 +0100

    returned pytest

--- pythontest/test_main.py
+++ pythontest/test_main.py
@@ -1,21 +1,16 @@
-#from main import *
-#from pythontest.main import bleu_implementation
-#import re
+from main import *
 
 
-
-    
-
-#def test_flattenlist():
-        #assert flatten_list()
-        #assert test_regex() == ("matched", matched)
-        #assert translator("bonjour") == ["hello"]
-        #assert keywords() == []
-        #assert clean("remove") == "remove"
-        #assert translate_document()
-        #assert bleu_implementation(["how are you heh?", "what should i eat?"], "bread is my pot") == 3.237552826118531e-78 
-        #assert check_word("")
-        #assert main() == 
+def test_translator():
+        assert translator(["bonjour"]) == ["good morning"]
 
 
+def test_flattenlist():
+        assert flatten_list([["a","b"],["c","d"]]) == ["a","b","c","d"]
 
+def test_clean():
+        assert clean("clea\nn") == "clean"
+        assert clean("clean \n clean") == "clean  clean"
+
+def test_bleu_implementation():
+        assert bleu_implementation(["how are you heh?", "what should i eat?"], "bread is my pot") == 3.237552826118531e-78
