d807ccc283dcea7ce7927d959466a0e0ec15b1f8
Mingtong Du <fe20687@bristol.ac.uk>
2022-04-20 17:21:25 +0100

    Add files via upload

---
+++ translateserver/config/plugin.js
@@ -0,0 +1,21 @@
+'use strict';
+
+exports.assets = {
+  enable: true,
+  package: 'egg-view-assets',
+};
+
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks',
+};
+
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize',
+};
+
+exports.cors = {
+  enable: true,
+  package: 'egg-cors',
+};
