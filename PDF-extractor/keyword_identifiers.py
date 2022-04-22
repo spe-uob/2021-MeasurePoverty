35b4ccec93592fb17acdb82b0aa8371c169ab61e
lipichak02 <78699807+lipichak02@users.noreply.github.com>
2022-04-22 20:31:22 +0100

    Add files via upload
    added firebase connection and upload code for adding QuestionID table,
    added question id dictionary to keyword identifiers file

--- PDF-extractor/keyword_identifiers.py
+++ PDF-extractor/keyword_identifiers.py
@@ -32,3 +32,40 @@ questions_to_keywords={
     "Have a suitable place to study or do homework?":["study","homework"],
     "An outdoor space in the neighbourhood where they can play safely?":["outdoor","play","safely"]
 }
+
+
+
+questionIDs={
+    1:"Can your whole household afford to go for a week’s annual holiday, away from home?",
+    2:"Can your household afford a meal with meat, chicken, fish(or vegetarian equivalent)?",
+    3:"Can your household afford an unexpected required expense(amount to be filled) and pay through its own resources?",
+    4:"Does your household have a telephone(fixed landline or mobile)?",
+    5:"Does your household have a color TV?",
+    6:"Does the household have a washing machine? ",
+    7:"Does your household have a car/van for private use? ",
+    8:"Do you have any of the following problems with your dwelling/accommodation? ",
+    9:"Can your household afford to keep its home adequately warm?",
+    10:"In your neighbourhood, how frequently do you encounter the following problems?",
+    11:"(Access to local public transport and to postal/banking services)How do you describe the access of your household to the following services?",
+    12:"Could you tell me if your household...(READ OUT)? Replace worn-out furniture? Has (access to) and internet connection?",
+    13:"Have a mobile phone?",
+    14:"Have two pairs of properly fitting shoes (including a pair of all-weather shoes)?",
+    15:"Replace worn-out clothes by some new (not second hand) ones?",
+    16:"Get-together with friends/family (relatives) for a drink/meal at least once a month?",
+    17:"Regularly participate in a leisure activity such as sport, cinema, concert,etc.?",
+    18:"Spend a small amount of money each week on yourself (without having to consult anyone)?",
+    19:"About how many times have you been to a GP or a specialist, excluding dentist and opthalmologist,during the last 12 months?",
+    20:"Have some new (not second hand) clothes?",
+    21:"Have two pairs of properly fitting shoes(including a pair of all-weather shoes?",
+    22:"Have fresh fruits and vegetables once a day?",
+    23:"Have three meals a day?",
+    24:"Have a meal with meat, chicken or fish (or vegetarian equivalent) at least once a day ",
+    25:"Have books at home suitable for their age?",
+    26:"Have outdoor leisure equipment (bicycle, roller skates,etc)?",
+    27:"Have indoor games (educational baby toys, building blocks, board games, computer games,etc.)?",
+    28:"Participate in a regular leisure activity (swimming, playing an instrument, youth organisations,etc.)?",
+    29:"Have celebrations on special occasions (birthdays,name days, religious events)?",
+    30:"Participate in school trips and school events that cost money?",
+    31:"Have a suitable place to study or do homework?",
+    32:"An outdoor space in the neighbourhood where they can play safely?"
+}
