49b22c4d797a59cea7a7b618c694c0acd9cfa42c
Fan Lei <vr19825@bristol.ac.uk>
2022-04-24 14:24:30 +0100

    add more keywords

--- PDF-extractor/test_main.py
+++ PDF-extractor/test_main.py
@@ -14,14 +14,9 @@ def test_groupedquestionsbykeywords():
 def test_remove_whitespace():
         assert remove_whitespace("clea\nn") == "clean"
         assert remove_whitespace("clean\n clean") == "clean clean"
-
-
-
 def test_removebrackets():
         in_dict= {"(question 111)Does your household have a car/van for private use? ":["van","car","vehicle"]}
         assert remove_brackets(in_dict) == {'Does your household have a car/van for private use? ': ['van', 'car', 'vehicle']}
-
-
 def test_checkkeywords():
         assert check_keywords({"How are you?":["you"],"Can your whole household afford to go for a week's annual holiday, away from home?":["vacation","holiday","holiday residence","residence"]})=={"Can your whole household afford to go for a week's annual holiday, away from home?": ['vacation', 'holiday', 'holiday residence', 'residence']}
 
--- PDF-extractor/text_preprocessing.py
+++ PDF-extractor/text_preprocessing.py
@@ -23,11 +23,20 @@ print(remove_brackets({"(question 111)Does your household have a car/van for pri
 ## new def check keywords
 def check_keywords(input_dictionary):
     relevant_questions =  defaultdict()
-    array_of_keywords = ["holiday","vacation","holiday residence","residence","vegetarian","expense","costs","telephone","phone","colour TV","colour television","washing machine", "van", "dwelling","lodging","warm","heat"]
+    array_of_keywords = ["holiday","vacation","holiday residence","residence","vegetarian","expense","costs",
+    "telephone","phone","colour TV","colour television","washing machine", "van", "dwelling","lodging","warm",
+    "heat","heating","warmth","problems","neighbourhood","encounter","postal","banking","services",
+    "mobile phone","phone","mobile","shoes","all-weather","fitting",
+    "second-hand","clothes","drink","meal","month","relatives","sport","cinema","concert","small",
+    "money","consult","small","money","consult","dentist","ophthalmologist","specialist","doctor",
+    "second hand","clothes","pairs","fitting shoes","all-weather","fruits","vegetables","three","meals"
+    ,"meat","chicken","fish","books","bicycle","roller skates","baby","toys","building blocks","board games",
+    "computer games","swimming","instrument","birthdays","name days","religious events","trips",
+    "school trips","school events","study","homework","outdoor","play","safely"]
     for question in input_dictionary.keys():
         if any(word in question for word  in array_of_keywords):
             relevant_questions[question] = input_dictionary[question]
     return relevant_questions
 
-print(check_keywords({"How are you?":["you"],"Can your whole household afford to go for a week's annual holiday, away from home?":["vacation","holiday","holiday residence","residence"]}))
-
+print(check_keywords({"An outdoor space in the neighbourhood where they can play safely?":["outdoor","play","safely"]
+,"My name is lisa":["lisa"]}))
