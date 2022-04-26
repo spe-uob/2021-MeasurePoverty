5f97b1ce282f7c65e059038f136e7723f58043ee
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2022-04-26 15:39:38 +0100

    germany table

--- deploy/database/2009/France/france2009.js
+++ deploy/database/2009/France/france2009.js
@@ -1,18 +1,18 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js'
-// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyASZ59fobXr6ovy8QQUX2mogFso22v5nQM",
-    authDomain: "measuredb.firebaseapp.com",
-    databaseURL: "https://measuredb-default-rtdb.firebaseio.com",
-    projectId: "measuredb",
-    storageBucket: "measuredb.appspot.com",
-    messagingSenderId: "298611251603",
-    appId: "1:298611251603:web:0d236cfa7d1926a552f066"
+  apiKey: "AIzaSyASZ59fobXr6ovy8QQUX2mogFso22v5nQM",
+  authDomain: "measuredb.firebaseapp.com",
+  databaseURL: "https://measuredb-default-rtdb.firebaseio.com",
+  projectId: "measuredb",
+  storageBucket: "measuredb.appspot.com",
+  messagingSenderId: "298611251603",
+  appId: "1:298611251603:web:0d236cfa7d1926a552f066"
 };
+
 
 // youtube link: https://www.youtube.com/watch?v=H8frPNjKSC8
 // Initialize Firebase
@@ -44,8 +44,52 @@ firebaseRef3.on("value" , function(snapshot){
                       `
     });
 })
+//this point onwards is table
+var stdNo = 0;
+var tbody = document.getElementById('tbody1');
 
-function myFunction() {
-    var element = elements.body;
-    element.classList.toggle("dark-mode");
-  }
+
+function AddItemToTable(hi, how, are, you) {
+    let trow = document.createElement("trow");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let td5 = document.createElement("td");
+
+    td1.innerHTML= ++stdNo;
+    td2.innerHTML= hi;
+    td3.innerHTML= how;
+    td4.innerHTML= are;
+    td5.innerHTML= you;
+
+    trow.appendChild(td1);
+    trow.appendChild(td2 );
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+    trow.appendChild(td5);
+}
+
+function AddAllItemsToTable(QuestionIDs) {
+    stdNo=0;
+    tbody.innerHTML="";
+    QuestionIDs.forEach(element => {
+        AddItemToTable(element.var())
+    });
+}
+
+// getting all data
+
+function GetAllDataOnce() {
+    const dbRef = ref(db);
+    get(child(dbRef, "QuestionIDs"))
+    .then((snapshot)=> {
+        var hi =[];
+        snapshot.forEach(childSnapshot => {
+            students.push(childSnapshot.val())  ;
+        });
+        AddAllItemsToTable(hi)
+    });
+}
+
+window.onload = GetAllDataOnce
--- deploy/database/2009/Germany/germany2009.js
+++ deploy/database/2009/Germany/germany2009.js
@@ -1,46 +1,86 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js'
-// Import the functions you need from the SDKs you need
+
+var stdNo = 0;
+var tbody = document.getElementById('tbody1');
+
+
+function AddItemToTable(name, roll, sec, gen) {
+    let trow = document.createElement("tr");
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    let td4 = document.createElement('td');
+    let td5 = document.createElement('td');
+
+    td1.innerHTML= ++stdNo;
+    td2.innerHTML= name;
+    td3.innerHTML= how;
+    td4.innerHTML= are;
+    td5.innerHTML= you;
+
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+    trow.appendChild(td5);
+
+    tbody.appendChild(trow);
+}
+
+function AddAllItemsToTable(QuestionIDs) {
+    stdNo=0;
+    tbody.innerHTML="";
+    QuestionIDs.forEach(element => {
+        AddItemToTable(element.QuestionIDs);
+    });
+}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyASZ59fobXr6ovy8QQUX2mogFso22v5nQM",
-    authDomain: "measuredb.firebaseapp.com",
-    databaseURL: "https://measuredb-default-rtdb.firebaseio.com",
-    projectId: "measuredb",
-    storageBucket: "measuredb.appspot.com",
-    messagingSenderId: "298611251603",
-    appId: "1:298611251603:web:0d236cfa7d1926a552f066"
+  apiKey: "AIzaSyASZ59fobXr6ovy8QQUX2mogFso22v5nQM",
+  authDomain: "measuredb.firebaseapp.com",
+  databaseURL: "https://measuredb-default-rtdb.firebaseio.com",
+  projectId: "measuredb",
+  storageBucket: "measuredb.appspot.com",
+  messagingSenderId: "298611251603",
+  appId: "1:298611251603:web:0d236cfa7d1926a552f066"
 };
 
-// youtube link: https://www.youtube.com/watch?v=H8frPNjKSC8
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var firebaseRef = firebase.database().ref("QuestionIDs")
-firebaseRef.on("value" , function(snapshot){
-    snapshot.forEach(function(element){
-        document.querySelector('#englishQuestions').innerHTML += `
-               <div>${element.val()}</div>
-               `
+const app = initializeApp(firebaseConfig);  
+import {getDatabase, ref, child, onValue, get}
+from "https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js";
+//getting all data
+
+function GetAllDataOnce() {
+    const dbRef = ref(db);
+    get(child(dbRef, "QuestionIDs"))
+        .then((snapshot)=> {
+            
+            var students =[];
+            
+            snapshot.forEach(childSnapshot => {
+                students.push(childSnapshot.val());
+            });
+            AddAllItemsToTable(students);
+        })
+}
+
+function GetAllDataRealTime() {
+    const dbRef = ref(db);
+
+    onValue(dbRef, (snapshot)=>{
+        var students =[];
+            
+            snapshot.forEach(childSnapshot => {
+                students.push(childSnapshot.val());
+            });
+            AddAllItemsToTable(students);
     });
-})
+    
+}
 
 
-var firebaseRef2 = firebase.database().ref("QuestionIDs")
-firebaseRef2.on("value" , function(snapshot){
-    snapshot.forEach(function(element){
-        document.querySelector('#questionID').innerHTML += `
-        <div>${element.key}</div>
-                      `
-    });
-})
-
-var firebaseRef3 = firebase.database().ref("Germany-2009")
-firebaseRef3.on("value" , function(snapshot){
-    snapshot.forEach(function(element){
-        document.querySelector('#germanQuestions').innerHTML += `
-        <div>${element.val()}</div>
-                      `
-    });
-})
+window.onload = GetAllDataOnce;
