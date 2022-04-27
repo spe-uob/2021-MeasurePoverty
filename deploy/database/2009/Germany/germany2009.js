e64114758c788b407928b32f5eebd86d73494d07
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2022-04-27 18:47:44 +0100

    moved javascript to js

--- deploy/database/2009/Germany/germany2009.js
+++ deploy/database/2009/Germany/germany2009.js
@@ -1,87 +1,78 @@
+ //https://www.youtube.com/watch?v=KnAsYNhI_CY
+        //javascript part to fill in the table
 
-var stdNo = 0;
-var tbody = document.getElementById('tbody1');
+        var QID = 0;
+        var tbody = document.getElementById('tbody1');
+
+        function AddItemToTable(english, foreign) {
+            let trow = document.createElement("tr");
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            let td3 = document.createElement('td');
+
+            td1.innerHTML= ++QID;
+            td2.innerHTML= english;
+            td3.innerHTML= foreign;
 
 
-function AddItemToTable(name, roll, sec, gen) {
-    let trow = document.createElement("tr");
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    let td3 = document.createElement('td');
-    let td4 = document.createElement('td');
-    let td5 = document.createElement('td');
+            trow.appendChild(td1);
+            trow.appendChild(td2);
+            trow.appendChild(td3);
 
-    td1.innerHTML= ++stdNo;
-    td2.innerHTML= name;
-    td3.innerHTML= how;
-    td4.innerHTML= are;
-    td5.innerHTML= you;
+            tbody.appendChild(trow);
+           // body.appendChild(trow);
+        }
 
-    trow.appendChild(td1);
-    trow.appendChild(td2);
-    trow.appendChild(td3);
-    trow.appendChild(td4);
-    trow.appendChild(td5);
-
-    tbody.appendChild(trow);
-}
-
-function AddAllItemsToTable(QuestionIDs) {
-    stdNo=0;
-    tbody.innerHTML="";
-    QuestionIDs.forEach(element => {
-        AddItemToTable(element.QuestionIDs);
-    });
-}
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyASZ59fobXr6ovy8QQUX2mogFso22v5nQM",
-  authDomain: "measuredb.firebaseapp.com",
-  databaseURL: "https://measuredb-default-rtdb.firebaseio.com",
-  projectId: "measuredb",
-  storageBucket: "measuredb.appspot.com",
-  messagingSenderId: "298611251603",
-  appId: "1:298611251603:web:0d236cfa7d1926a552f066"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);  
-import {getDatabase, ref, child, onValue, get}
-from "https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js";
-//getting all data
-
-function GetAllDataOnce() {
-    const dbRef = ref(db);
-    get(child(dbRef, "QuestionIDs"))
-        .then((snapshot)=> {
-            
-            var students =[];
-            
-            snapshot.forEach(childSnapshot => {
-                students.push(childSnapshot.val());
-            });
-            AddAllItemsToTable(students);
-        })
-}
-
-function GetAllDataRealTime() {
-    const dbRef = ref(db);
-
-    onValue(dbRef, (snapshot)=>{
-        var students =[];
-            
-            snapshot.forEach(childSnapshot => {
-                students.push(childSnapshot.val());
-            });
-            AddAllItemsToTable(students);
-            print(students);
-    });
+        
     
-}
 
 
-window.onload = GetAllDataOnce;
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyASZ59fobXr6ovy8QQUX2mogFso22v5nQM",
+    authDomain: "measuredb.firebaseapp.com",
+    databaseURL: "https://measuredb-default-rtdb.firebaseio.com",
+    projectId: "measuredb",
+    storageBucket: "measuredb.appspot.com",
+    messagingSenderId: "298611251603",
+    appId: "1:298611251603:web:0d236cfa7d1926a552f066"
+  };
+    
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+        import { getDatabase, ref, child, onValue, get }
+        from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+        const db = getDatabase();
+
+         //adding items to the table, this function is fine
+        function AddAllItemsToTable(Question){    
+            QID =0;
+            tbody.innerHTML="";
+            Question.forEach(element => {
+                AddItemToTable(element.english, element.german)
+            });
+        }
+        //working on everything else below here
+
+        //getting data once
+        function GetAllDataOnce() {
+            const dbRef = ref(db);
+
+            get(child(dbRef, "Questionnaire"))
+            .then((snapshot)=>{
+                var questions =[];
+                
+                snapshot.forEach(childSnapshot => {
+                    questions.push(childSnapshot.val());
+                });
+                AddAllItemsToTable(questions);
+            });
+        }
+
+
+        window.onload = GetAllDataOnce;
