f4bca93260ec4e4aa8cd0ac2209bbd73db8e154b
Elicit3495 <78478782+Elicit3495@users.noreply.github.com>
2022-05-06 15:31:05 +0100

    test

--- PDF-extractor/test_main.py
+++ PDF-extractor/test_main.py
@@ -14,7 +14,7 @@ def test_remove_whitespace():
         assert remove_whitespace("clea\nn") == "clean"
         assert remove_whitespace("clean\n clean") == "clean clean"
 
-'''
+
 def test_removebrackets():
         in_dict= {"(question 111)Does your household have a car/van for private use? ":["van","car","vehicle"]}
         assert remove_brackets(in_dict) == {'Does your household have a car/van for private use? ': ['van', 'car', 'vehicle']}
@@ -22,6 +22,7 @@ def test_removebrackets():
 def test_checkkeywords():
         assert check_keywords({"How are you?":["you"],"Can your whole household afford to go for a week's annual holiday, away from home?":["vacation","holiday","holiday residence","residence"]})=={"Can your whole household afford to go for a week's annual holiday, away from home?": ['vacation', 'holiday', 'holiday residence', 'residence']}
 
+'''
 #functions from translators.py
 def test_translatorintoenglish():
         assert translator_into_english("Ich möchte diesen Text übersetzen!") == "I want to translate this text!"
